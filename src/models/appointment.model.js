0145ed03374ac06bcc954ff163969352593846f3
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-05-11 23:35:40 +0530

    added appointment.model.js

---
+++ src/models/appointment.model.js
@@ -0,0 +1,48 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+const appointmentschema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true,
+    },
+    doctorId:{
+        type:String,
+        required:true,
+    },
+    doctorInfo:{
+        type:String,
+        required:true,
+    },
+    userInfo:{
+        type:String,
+        required:true,
+    },
+    date:{type:Date,required:true},
+    status:{
+        type:String,
+        required:true,
+        default:'pending',
+    },
+    time:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true })
+
+//define a joi schema---->
+const appointJoischema = Joi.object({
+    userId:Joi.string().required(),
+    doctorId:Joi.string().required(),
+    doctorInfo:Joi.string().required(),
+    userInfo:Joi.string().required(),
+    date:Joi.date().required(),
+    status:Joi.string().required().default('pending'),
+    time:Joi.string().required(),
+}).options({stripUnknown:true});
+
+appointmentschema.valiadateAppointment = async function(){
+    return appointJoischema.validateAsync(this.toObject());
+}
+
+module.exports.appointment = mongoose.model('appointment',appointmentschema);
